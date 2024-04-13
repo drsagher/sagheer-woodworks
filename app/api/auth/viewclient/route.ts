20771de570eb0c4d9681231c0b31ae55d3eb3341
Abubakar Siddique <dr.sagher@gmail.com>
2024-04-13 06:59:06 -0700

    Update Client V6

--- app/api/auth/viewclient/route.ts
+++ app/api/auth/viewclient/route.ts
@@ -1,6 +1,6 @@
 import { sql } from '@vercel/postgres';
 import {NextResponse} from "next/server";
-export async function PUT() {
+export async function GET() {
     const { rows, fields }  = await sql`select * from clients order by id`;
     return NextResponse.json(rows);
 }
